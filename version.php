e9d6100e0f25902aac1d4a5b2333a460bbdba33e
MooPlugins <manoj@bitkea.com>
2026-06-29 17:45:37 +0530

    Update version.php

--- version.php
+++ version.php
@@ -25,13 +25,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026061901;
-$plugin->requires = 2022112800;
 $plugin->component = 'tiny_videolesson';
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';
+$plugin->version = 2026061902;
+$plugin->requires = 2024042212;
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->dependencies = [
-'mod_videolesson' => 2026061900,
-'filter_videolesson' => 2026061900,
+'mod_videolesson' => 2026061901,
+'filter_videolesson' => 2026061901,
 ];
